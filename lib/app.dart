c25143bd5803bdad6bf66c0dcd9847a9d7619aab
Deng Wuor Joak <dengjoak@gmail.com>
2019-11-16 08:49:04 +0100

    finished

--- lib/app.dart
+++ lib/app.dart
@@ -1,17 +1,18 @@
-import "package:flutter/material.dart";
-import "screens/home.dart";
-class App extends StatelessWidget {
+import 'package:flutter/material.dart';
+import 'screens/home.dart';
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return: MaterialApp(
-      title: "Flutter Web Views",
+    return MaterialApp(
+      title: 'Flutter Web Views',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
-        fontFamily: "Arial",
-        textTheme: TextTheme(
-          button: TextStyle(color: Colors.white, fontSize: 18.0),
-          title: TextStyle(color: Colors.red),
-          home: Home(),
-       
+          primarySwatch: Colors.blue,
+          fontFamily: "Arial",
+          textTheme: TextTheme(
+              button: TextStyle(color: Colors.white, fontSize: 18.0),
+              title: TextStyle(color: Colors.red))),
+      home: Home(),
+    );
   }
 }
